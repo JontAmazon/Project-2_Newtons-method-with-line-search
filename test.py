6d04cfcfaa9e09e0253c77a28ecaf0e4dd9b3f42
fritiof <fritiofh@gmail.com>
2019-09-27 12:16:13 +0200

    test class now works

--- test.py
+++ test.py
@@ -14,16 +14,22 @@ class test_classic_newton(unittest.TestCase):
 
 class test_gradient(unittest.TestCase):
     
-    def setup(self):
-        x = [4, 2]
-        f= 100*(x[1] - x[0]**2)**2 + (1 - x[0])**2
+    def setUp(self):
+        x = np.array([4., 2.])
+        def f(x):
+            return 100*(x[1] - x[0]**2)**2 + (1 - x[0])**2
         problem = op.Problem(f, 2)
         solver = op.Solver(problem)
         self.gradient = solver.compute_gradient(x)
+        g1 = self.gradient
         self.hessian = solver.compute_hessian(x)
         self.grad = np.array([100*(4*x[0]**3-4*x[0]*x[1])-2+2*x[0],100*(2*x[1]-2*x[0]**2)])
-    def test(self):
-        self.assertAlmostEqual(self.grad,self.gradient) 
+        g2= self.grad
+        self.diff = lg.norm(self.gradient-self.grad)/lg.norm(self.grad)
+    def test_dimension(self):
+        self.assertEqual(np.shape(self.gradient),(2,))
+    def test_correct_value(self):
+        self.assertAlmostEqual(self.diff,0,4) #'not almost equal'
         
 if __name__ =='__main__':
     unittest.main()
